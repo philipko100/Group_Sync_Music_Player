6d92068cf6e682269c4c119bc6e1841edcbedd57
Yungu Philip Ko <philip.ko.100@gmail.com>
2021-09-08 01:32:26 -0700

    added home page material UI and redirect to room page if session is already is a room

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from rest_framework import generics, serializers, status
 from .serializer import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -67,3 +69,12 @@ class CreateRoomView(APIView):
                 self.request.session['room_code'] = room.code
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
             return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
